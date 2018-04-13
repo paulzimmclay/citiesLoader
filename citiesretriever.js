9294d3af20c7848a1f01eb84207a03139571188b
pzc <paul.zimmerman.clayton@gmail.com>
2018-04-13 14:15:29 -0500

    added retriever

---
+++ citiesretriever.js
@@ -0,0 +1,4 @@
+// Get database from local storage
+// Get "CitiesDatabase" from local storage, parse, assign to variable CitiesDatabase
+
+const CitiesDatabase = JSON.parse(localStorage.getItem("CitiesDatabase"))
