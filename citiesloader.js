974c8a41376ce5a539936a75a55f260b0f622f1f
pzc <paul.zimmerman.clayton@gmail.com>
2018-04-14 20:32:46 -0500

    pushing all elements for example to dom

--- citiesloader.js
+++ citiesloader.js
@@ -10,10 +10,22 @@ const cityRef = document.querySelector("#cityCards")
 
 // Create card title in H2
 const cardTitle = document.createElement("h2")
-const titleContent = document.createTextNode(CitiesDatabase.northAmerica[0].name)
-cardTitle.appendChild(titleContent)
 cityRef.appendChild(cardTitle)
+cardTitle.textContent = CitiesDatabase.northAmerica[0].name
+
 // Create card image in img tag
 const cardImage = document.createElement("img")
-
 cityRef.appendChild(cardImage)
+cardImage.setAttribute("width", "100%")
+cardImage.setAttribute("src", CitiesDatabase.northAmerica[0].image)
+
+// Create year visited
+const cardYear = document.createElement("h3")
+cityRef.appendChild(cardYear)
+cardYear.textContent = CitiesDatabase.northAmerica[0].year_visited
+
+// Create List of things to do
+const cardList = document.createElement("ol")
+CitiesDatabase.northAmerica[0].top_five_attractions.forEach( (thingToDo) => 
+  console.log(thingToDo)  
+)
